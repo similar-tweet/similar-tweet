eb0c87276756d488e45a084c8b72c7d1ea4b39b3
similar-tweet <74700452+similar-tweet@users.noreply.github.com>
2020-11-19 16:35:18 +0530

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
+  console.log('hello!')
+  console.log(req, res);
   res.statusCode = 200
   res.json({ name: 'John Doe' })
-  console.log(req, res);
+
   return res;
 }
